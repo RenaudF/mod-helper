627c214d85be733fd383f2d80226ae552018801a
RenaudF <sirgzu@hotmail.com>
2021-06-07 14:49:00 +0100

    scenario: added moral boost

---
+++ examples/moral-boost.ts
@@ -0,0 +1,17 @@
+import fs from "fs";
+import { descriptors, parse, serialise } from "lib";
+
+try {
+  const data = fs.readFileSync(descriptors.unit.backup, "utf8");
+  const models = parse(data);
+  // prettier-ignore
+  // give everyone a moral boost
+  models.forEach((model) => {
+    const { stat_mental: [moral] } = model;
+    model.stat_mental[0] = moral + 4 + Math.ceil(moral/4);
+  });
+  const serialised = serialise(models);
+  fs.writeFileSync(descriptors.unit.active, serialised);
+} catch (err) {
+  console.error(err);
+}

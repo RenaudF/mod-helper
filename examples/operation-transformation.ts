bd6de54ca875643974ea1f9cbea0b16512b47a7b
RenaudF <sirgzu@hotmail.com>
2021-06-07 14:36:46 +0100

    scenario: fix missing is_female flags on some female units

--- examples/operation-transformation.ts
+++ examples/operation-transformation.ts
@@ -1,11 +1,14 @@
 import fs from "fs";
-import { descriptors, identity, parse, serialise } from "lib";
+import { descriptors, parse, serialise } from "lib";
 
 try {
   const data = fs.readFileSync(descriptors.unit.backup, "utf8");
   const models = parse(data);
-  const updated = models.map(identity);
-  const serialised = serialise(updated);
+  // fix missing is_female tags on some female units
+  models
+    .filter(({ type: [_type] }) => _type.includes("female"))
+    .forEach((model) => (model.is_female = true));
+  const serialised = serialise(models);
   fs.writeFileSync(descriptors.unit.active, serialised);
 } catch (err) {
   console.error(err);
